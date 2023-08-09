a69c13e2d5f59571decadc12abcda5016759bee6
VaneSanchez <nyagami@yahoo.com>
2023-08-09 13:55:39 -0300

    adding features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
-import { FaTwitterSquare, FaLinkedin, FaReact } from 'react-icons/fa';
+import { FaTwitterSquare, FaLinkedin, FaReact, FaGithub } from 'react-icons/fa';
 import { HiEnvelope } from "react-icons/hi2";
 import fotoport from '../public/images/fotoport.png';
+import miCv from '/public/bvsp.pdf';
 
 function App() {
- 
+
+  const descargarCv = () => {
+    window.location.href = miCv;
+  };
  
   return (
     <div className="font-sans bg-stone-900 text-white">
-      <header className="bg-stone-800 text-cyan-600 py-4 px-6">
-        <h1 className="text-2xl font-bold">My Portfolio</h1>
+
+      
+
+      <header className="bg-stone-800 text-cyan-600 py-4 px-6 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Bienvenidos!</h1>
+        <button onClick={descargarCv} className="bg-violet-700 hover:bg-violet-800 text-white py-2 px-4 rounded-full inline-flex items-center">
+          <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+          <span>Download CV</span>
+        </button>
       </header>
 
 {/* ------------------PRESENTACION---------------------- */}
@@ -26,7 +37,7 @@ function App() {
           />
           <div className="flex-1">
             <h3 className="text-2xl text-amber-400 font-medium mb-5">Vanesa Sánchez</h3>
-            <p className="text-sm mb-4 text-stone-50">Soy programadora con gusto por el Front.<br />Egresada de CaC en Full Stack Java, React y Spring.</p> 
+            <p className="text-sm mb-4 text-stone-50">Programadora con gusto por el Front.<br />Egresada de CaC en Full Stack Java, React y Spring.</p> 
             
             <p className="my-4 font-bold">Mi stack:</p>
             <div className="flex justify-center items-center">
@@ -46,16 +57,16 @@ function App() {
         </div>
 
 
-{/* ------------------PROYECTOS---------------------- */}
+{/* ------------------PROYECTOS INDIVIDUALES---------------------- */}
 
   
-          <h2 className="text-2xl text-rose-500 font-bold mt-8 mb-4">My Projects</h2>
+          <h2 className="text-2xl text-rose-500 font-bold mt-8 mb-8">Proyectos Individuales</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
 
 
             <div className="bg-stone-800 border border-stone-600 rounded p-4">
-              <h3 className="text-lg font-bold mb-2">Project 1</h3>
+              <h3 className="text-lg font-bold mb-2">To do List</h3>
               <p className="mb-2">Lista de Tareas simple con CRUD  y login.</p>
 
               <div>
@@ -70,7 +81,7 @@ function App() {
             </div>
 
             <div className="bg-stone-800 border border-stone-600 rounded p-4">
-              <h3 className="text-lg font-bold mb-2">Project 2</h3>
+              <h3 className="text-lg font-bold mb-2">Humaya</h3>
               <p className="mb-2">Landing Page de Ventas</p>
               <div>
                 <a href="https://peppy-croissant-5e5c96.netlify.app/" target="_blank" rel="noopener noreferrer">
@@ -84,7 +95,7 @@ function App() {
             </div>
 
             <div className="bg-stone-800 border border-stone-600 rounded p-4">
-              <h3 className="text-xl font-bold mb-2">Project 3</h3>
+              <h3 className="text-xl font-bold mb-2">Buscador</h3>
               <p className="mb-2">Buscador de películas con API.</p>
               <div>
                 <a href="https://relaxed-meringue-4f67b8.netlify.app/" target="_blank" rel="noopener noreferrer">
@@ -98,7 +109,7 @@ function App() {
             </div>
 
             <div className="bg-stone-800 border border-stone-600 rounded p-4">
-              <h3 className="text-lg font-bold mb-2">Project 4</h3>
+              <h3 className="text-lg font-bold mb-2">Llama No Drama</h3>
               <p className="mb-2">Animación simple con CSS.</p>
               <div>
                 <a href="https://roaring-dango-723e2e.netlify.app/" target="_blank" rel="noopener noreferrer">
@@ -111,8 +122,47 @@ function App() {
               </div>
             </div>
 
+          </div>
+
+          {/* ------------------PROYECTOS GRUPALES---------------------- */}
+
+  
+          <h2 className="text-2xl text-rose-500 font-bold mt-8 mb-8">Proyectos Grupales</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
+
+
+            <div className="bg-stone-800 border border-stone-600 rounded p-4">
+              <h3 className="text-lg font-bold mb-2">TecnoTienda</h3>
+              <p className="mb-2">Tienda en Línea.</p>
+
+              <div>
+                <a href="https://tecnocompu-23302.web.app/" target="_blank" rel="noopener noreferrer">
+                  <img
+                    src="./images/proyect6.png"
+                    alt="Vista previa de ejemplo"
+                    className="link-image"
+                  />
+                </a>
+              </div>
+            </div>
+
+            <div className="bg-stone-800 border border-stone-600 rounded p-4">
+              <h3 className="text-lg font-bold mb-2">PokeAPI</h3>
+              <p className="mb-2">Listado de pokemones.</p>
+              <div>
+                <a href="https://dancing-bubblegum-82c624.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <img
+                    src="./images/proyect5.png"
+                    alt="Vista previa de ejemplo"
+                    className="link-image"
+                    />
+                </a>
+            </div>
+            </div>
 
           </div>
+
         </div>
       </main>
 
@@ -133,17 +183,20 @@ function App() {
 
                       
             <div className='mx-5 flex justify-center space-x-4'>
-              <a href='mailto:bvanesanchezp@gmail.com'>
-                <HiEnvelope size={30}/>
-              </a>
-              
+
               <a href='https://twitter.com/QtieAlpaca'>
                 <FaTwitterSquare size={30}/>
               </a>
-              
+              <a href='https://github.com/NezziY'>
+                <FaGithub size={30}/>
+              </a>
               <a href='https://www.linkedin.com/in/bertha-vanessa-s%C3%A1nchez-posadas-5b05a9208/'>
                 <FaLinkedin size={30}/>
               </a>
+              <a href='mailto:bvanesanchezp@gmail.com'>
+                <HiEnvelope size={30}/>
+              </a>
+
             </div>
               <p>React Developer</p>
           </div>
