9f3cf09343258a5148222157ade51c739ee43840
VaneSanchez <nyagami@yahoo.com>
2024-02-16 12:58:58 -0300

    edit skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,13 +14,13 @@ function Skills() {
     <div>
       <p className="my-5 text-2xl font-bold text-center">Skills:</p>
       <div className="flex flex-col justify-center items-center">
-        <div className="grid grid-cols-5 gap-2 items-center">
+        <div className="grid grid-cols-4 gap-2 items-center">
           <img src={htmlImage} sizes={12} />
           <img src={cssImage} sizes={12} />
           <img src={jsImage} width={48} />
           <img src={reactImage} width={42} />
-          <img src={node} width={40} />
-          <img src={sql} width={46} />
+          {/* <img src={node} width={40} /> */}
+          {/* <img src={sql} width={46} /> */}
           <img src={firebaseImage} width={48} />
           <img src={tailwindImage} width={48} />
           <img src={bootsT} width={34} />
